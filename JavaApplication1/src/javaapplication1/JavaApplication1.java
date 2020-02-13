fc950fa396f516c1a85cab90bf655e956eebbae6
ranvi-singh <60109297+ranvi-singh@users.noreply.github.com>
2020-02-13 13:17:09 -0500

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-name :ranvir
+name :ranvir singh
  */
 
 package javaapplication1;
